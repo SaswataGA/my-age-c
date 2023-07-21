3c7487a8a4faf5ea7c4c00703f88aa62dd8c3b15
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-07-21 12:54:12 +0600

    Add files via upload

---
+++ myage.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   int myage;
+
+// Ask the user to type a number
+printf("What is your age?: \n");
+
+// Get and save the number the user types
+scanf("%d", &myage);
+
+// Output the number the user typed
+printf("Your age is: %d", myage);
+    return 0;
+}
